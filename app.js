4a1e0f77dd0bb9ae7867650ba7bf50c3aa1094f8
swpv <swpv@email.cz>
2025-12-05 19:52:55 +0000

    test

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // ===== CORS pro /api =====
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Origin', 'https://tvuj-frontend.onrender.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
